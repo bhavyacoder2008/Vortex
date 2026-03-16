51ee224605b287f9f981abb628a8873ee68abc0d
bhavyacoder2008 <bhavyamanochaviiilily@gmail.com>
2026-03-17 00:51:35 +0530

    new commit

--- backend/src/utils/sendOtp.js
+++ backend/src/utils/sendOtp.js
@@ -8,7 +8,7 @@ const sendOtp = async (email, otp) => {
     console.log(email)
 
     const res = await resend.emails.send({
-        from: "onboarding@resend.dev", 
+        from: "vortex@bhavya.alactichost.net", 
         to: email,
         subject: "Your OTP for verification",
         html: `<p>Your OTP is <h1>${otp}</h1></p>`
