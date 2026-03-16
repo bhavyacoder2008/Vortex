74f570018d158c423a2d642642d328ef2b1b53f9
bhavyacoder2008 <bhavyamanochaviiilily@gmail.com>
2026-03-16 23:05:35 +0530

    cryingEmoji

--- backend/src/utils/sendOtp.js
+++ backend/src/utils/sendOtp.js
@@ -3,12 +3,17 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOtp = async (email, otp) => {
-    await resend.emails.send({
+
+
+    console.log(email)
+
+    const res = await resend.emails.send({
         from: "onboarding@resend.dev", 
         to: email,
         subject: "Your OTP for verification",
         html: `<p>Your OTP is <h1>${otp}</h1></p>`
     });
+    console.log(res)
     console.log("OTP Sent");
 }
 
