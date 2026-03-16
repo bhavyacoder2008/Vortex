978d16f21bea6bfefcf3fb3ba00ba179243ee1bc
bhavyacoder2008 <bhavyamanochaviiilily@gmail.com>
2026-03-16 22:54:01 +0530

    Switched to resend

--- backend/src/utils/sendOtp.js
+++ backend/src/utils/sendOtp.js
@@ -1,34 +1,15 @@
-import nodemailer from "nodemailer"
+import { Resend } from "resend";
 
-const sendOtp = async (email,otp) => {
-    try{console.log(process.env.EMAIL)
-    console.log(process.env.EMAIL_PASS)
-    const transporter = nodemailer.createTransport({
-        host : "smtp.gmail.com",
-        port : 587,
-        secure : false,
-        auth: {
-            user : process.env.EMAIL,
-            pass : process.env.EMAIL_PASS
-        },
-        family : 4
-    })
-    const mailOptions = {
-        from : process.env.EMAIL,
-        to : email,
-        subject : "Your OTP for verification",
-        html : `
-                <p>Your OTP is <h1>${otp}</h1></p>
-            `
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-    }
-
-    await transporter.sendMail(mailOptions);}
-    catch(err){
-        console.log(err)
-    }
-
-
+const sendOtp = async (email, otp) => {
+    await resend.emails.send({
+        from: "onboarding@resend.dev", 
+        to: email,
+        subject: "Your OTP for verification",
+        html: `<p>Your OTP is <h1>${otp}</h1></p>`
+    });
+    console.log("OTP Sent");
 }
 
 export default sendOtp;
