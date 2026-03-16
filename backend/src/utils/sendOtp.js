d9ac9a4311260e4c118fce7de07d76d198c30fa9
bhavyacoder2008 <bhavyamanochaviiilily@gmail.com>
2026-03-16 22:11:51 +0530

    jadu tona jis se hopefully chal jaye

--- backend/src/utils/sendOtp.js
+++ backend/src/utils/sendOtp.js
@@ -1,24 +1,26 @@
-import sgMail from '@sendgrid/mail';
+import nodemailer from "nodemailer"
 
-const sendOtp = async (email, otp) => {
-    console.log("SENDGRID_API_KEY env set:", process.env.SENDGRID_API_KEY ? "yes" : "no");
+const sendOtp = async (email,otp) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user : process.env.EMAIL,
+            pass : process.env.EMAIL_PASS
+        }
+    })
+    const mailOptions = {
+        from : process.env.EMAIL,
+        to : email,
+        subject : "Your OTP for verification",
+        html : `
+                <p>Your OTP is <h1>${otp}</h1></p>
+            `
 
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-    const msg = {
-        to: email,
-        from: process.env.EMAIL, // Make sure this is verified in SendGrid
-        subject: 'Your Vortex account OTP',
-        text: `OTP to verify your E-mail is ${otp}, enter this otp to verify your e-mail...`,
-    };
-
-    try {
-        const result = await sgMail.send(msg);
-        console.log("OTP sent successfully to", email, "SendGrid response:", result);
-    } catch (error) {
-        console.error("Error sending OTP:", error.message);
-        throw error;
     }
-};
+
+    await transporter.sendMail(mailOptions);
+
+
+}
 
 export default sendOtp;
