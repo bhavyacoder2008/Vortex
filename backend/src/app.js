cfa594b97c79a8ca8b1d25d948ebbd28987beb60
bhavyacoder2008 <bhavyamanochaviiilily@gmail.com>
2026-03-20 12:18:18 +0530

    Hopefully guest feature fully implemented

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://vortex-blue-eight.vercel.app",
     credentials : true
 }))
 app.use(express.json());
